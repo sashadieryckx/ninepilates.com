1f040cb637c09abf354dca6a12296500a83c5c64
sashadieryckx <sashadieryckx@hotmail.com>
2025-07-22 07:45:42 -0400

    added .json files for lang

---
+++ frontend/src/components/home/AccoladesSection.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="accolades-section">
+    <div class="accolade">
+      <h4>Accolade 1</h4>
+    </div>
+    <div class="accolade">
+      <h4>Accolade 2</h4>
+    </div>
+    <div class="accolade">
+      <h4>Accolade 3</h4>
+    </div>
+  </div>
+</template>
+<style scoped></style>
