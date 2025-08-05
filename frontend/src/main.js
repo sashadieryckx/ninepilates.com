ed969ef2d354ceed8ccbe012d400b358f3242a51
sashadieryckx <sashadieryckxmgmt@gmail.com>
2025-08-05 19:57:46 -0400

    added local json

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,8 +7,22 @@ import App from './App.vue'
 import router from './router'
 
 import LenisVue from 'lenis/vue'
-
 import { createHead } from '@unhead/vue/client'
+
+import { createI18n } from 'vue-i18n'
+import en from './locales/en.json'
+import fr from './locales/fr.json'
+
+const i18n = createI18n({
+  legacy: false,
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: {
+    en,
+    fr,
+  },
+})
+
 
 import '@splinetool/viewer'
 
@@ -20,4 +34,5 @@ app.use(pinia)
 app.use(router)
 app.use(LenisVue)
 app.use(head)
+app.use(i18n)
 app.mount('#app')
